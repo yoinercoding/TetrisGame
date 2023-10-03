51ae938fe637a8f18e0773bbf56cb75552e98127
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-10-03 14:45:18 -0300

    refactor

---
+++ consts.js
@@ -0,0 +1,10 @@
+export const BLOCK_SIZE = 20
+export const BOARD_WIDTH = 15
+export const BOARD_HEIGHT = 30
+
+export const EVENT_MOVEMENTS = {
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    DOWN: 'ArrowDown',
+    UP: 'ArrowUp',
+}
