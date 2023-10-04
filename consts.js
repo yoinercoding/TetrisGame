4c2f5d40d905ebb54607a2aeb525352b0e6c962e
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-10-04 09:53:25 -0300

    modified board height

--- consts.js
+++ consts.js
@@ -1,6 +1,6 @@
 export const BLOCK_SIZE = 20
 export const BOARD_WIDTH = 15
-export const BOARD_HEIGHT = 30
+export const BOARD_HEIGHT = 29
 
 export const EVENT_MOVEMENTS = {
     LEFT: 'ArrowLeft',
